bd284dd7468c1927899395ea6ecafa0f30174171
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-04 20:51:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,10 +4,9 @@ def my_collect(collection)
   array = []
    i=0
   while i < collection.length
-    new_collect = yield(collection[i]) 
+    new_collect = yield(collection[i])
     array << new_collect
       i+=1
    end
-   array 
+   array
 end
-
